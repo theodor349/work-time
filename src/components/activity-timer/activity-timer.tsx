95d09f19fbb4b3272901448674f693aba416c125
Theodor Risager <theodor349@gmail.com>
2024-02-21 22:06:20 +0100

    Do not log date

--- src/components/activity-timer/activity-timer.tsx
+++ src/components/activity-timer/activity-timer.tsx
@@ -36,7 +36,6 @@ export const ActivityTimer = ({ className, inputActivity, selectedDateTime, url 
 
     const getDate = function (): string {
         const date = selectedDateTime;
-        console.log(date);
         return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate() + " " + date.getHours() + "." + date.getMinutes() + "." + date.getSeconds();
     }
 
